1fad847d09740cb7d1139929321c58068ac67c68
vangyong <vangyong@126.com>
2018-11-19 20:47:41 +0800

    修改

---
+++ src/components/sonparent/parent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        父组建
+        <input type="text" v-model="num"/>
+        <Child @sendmsg="getMsg" :num="getNum"/>
+        {{info}}
+    </div>
+</template>
+
+<script>
+    import Child from "./child";
+
+    export default {
+        name: "parent",
+        data() {
+            return {
+                info: "",
+                num: 5
+            }
+        },
+        computed: {
+            getNum() {
+                return this.num - 0;
+            }
+        },
+        components: {Child},
+        methods: {
+            getMsg(data){
+                this.info=data;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
